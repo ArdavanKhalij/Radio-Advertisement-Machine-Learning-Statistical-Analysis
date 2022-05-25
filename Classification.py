bfea7036a0df008ace23dbacec8424e534fb2d9b
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-05-25 18:43:28 +0200

    Load the csv data and give column names

--- Classification.py
+++ Classification.py
@@ -3,7 +3,10 @@ import sklearn
 import pandas as pd
 import matplotlib
 
-# Load dataset
+# Convert txt to csv
+# read_file = pd.read_csv(r'Skin_NonSkin.txt', delimiter='\t')
+# read_file.to_csv(r'Skin_NonSkin.csv', header=None)
+
+# Load the csv data and give column names
 column_names = ['B', 'G', 'R', 'Class']
-read_file = pd.read_csv(r'Skin_NonSkin.txt')
-read_file.to_csv(r'Skin_NonSkin.csv', index=None)
+Data = pd.read_csv('Skin_NonSkin.csv', names=column_names, header=None)
