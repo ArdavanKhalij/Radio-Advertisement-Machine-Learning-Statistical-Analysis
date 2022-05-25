0523bd806240d45e5b8a4116b1983d75966f6f28
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-05-25 18:17:50 +0200

    Convert txt dataset to csv

--- Classification.py
+++ Classification.py
@@ -1,3 +1,9 @@
 # Libraries
 import sklearn
+import pandas as pd
 import matplotlib
+
+# Load dataset
+column_names = ['B', 'G', 'R', 'Class']
+read_file = pd.read_csv(r'Skin_NonSkin.txt')
+read_file.to_csv(r'Skin_NonSkin.csv', index=None)
