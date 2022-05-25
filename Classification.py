8777f5821d8dafcbbbde8fd09888ed3a14c85cbf
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-05-25 19:51:27 +0200

    Update Classification.py
    Naive Bayes classification

--- Classification.py
+++ Classification.py
@@ -1,5 +1,6 @@
 # Libraries
 from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 import matplotlib
 
@@ -12,11 +13,19 @@ import matplotlib
 column_names = ['B', 'G', 'R', 'Class']
 Data = pd.read_csv('Skin_NonSkin.csv', names=column_names, header=None)
 
-# Naive Bayes classification
-nb = GaussianNB()
+# Separate predictors and outcome
 excludeColumns = 'Class'
 predictors = [s for s in Data.columns if s not in excludeColumns]
 outcome = ['Class']
 X = Data[predictors]
 Y = Data[outcome]
+
+# Naive Bayes classification
+nb = GaussianNB()
 nb.fit(X, Y)
+# y_pred = nb.predict(X_test)
+
+# Naive Bayes classification
+dt = DecisionTreeClassifier(criterion='entropy', random_state=0)
+dt.fit(X, Y)
+# y_pred = dt.predict(X_test)
