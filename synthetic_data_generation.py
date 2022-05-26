eb776109f3fff96f2ea53ccab9999045c9aab140
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-05-26 21:57:24 +0200

    Done!

--- synthetic_data_generation.py
+++ synthetic_data_generation.py
@@ -6,7 +6,7 @@ import matplotlib. pylab as plt
 N = 10000
 
 # Generate data
-# Choose the line y=x, if y›x, then y=1, else y=0
+# Choose the line x=t, y=t, z=t, if y›x, then y=1, else y=0
 X = np.random.randint(0, 250, (N, 3))
 Y = np.ones(N)
 
