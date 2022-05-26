6fdba1f37338d50b2a6083a8bf234c079dd23a1c
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-05-26 21:16:49 +0200

    Update synthetic_data_generation.py

--- synthetic_data_generation.py
+++ synthetic_data_generation.py
@@ -3,4 +3,24 @@ import numpy as np
 import matplotlib. pylab as plt
 
 # Number of data
-N = 10
+N = 10000
+
+# Generate data
+# Choose the line y=x, if y›x, then y=1, else y=0
+X = np.random.randint(0, 250, (N, 3))
+Y = np.ones(N)
+
+# Label the data
+Y[X[:, 0] <= X[:, 1]] = 0
+
+# Flip the labels make the data non-separable
+for i in Y:
+    r = np.random.uniform(0, 1)
+    if r <= 0.1:
+        if i == 0:
+            i = 1
+        else:
+            i = 0
+
+print(len(X))
+print(Y)
